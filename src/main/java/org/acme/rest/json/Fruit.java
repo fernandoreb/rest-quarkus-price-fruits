d67a6b1dca4c4238c247a4110457289cb1beffb9
Fernando Guimarães <fguimara@redhat.com>
2023-09-21 09:00:50 -0300

    price-service

---
+++ src/main/java/org/acme/rest/json/Fruit.java
@@ -0,0 +1,15 @@
+package org.acme.rest.json;
+
+public class Fruit {
+
+    public String name;
+    public String price;
+
+    public Fruit() {
+    }
+
+    public Fruit(String name, String price) {
+        this.name = name;
+        this.price = price;
+    }
+}
